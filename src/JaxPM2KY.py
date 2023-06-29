4d6404c67d785df1b64e336d7bcbe8a6cd6272a5
chihway <chihway54@gmail.com>
2023-06-29 09:28:41 -0500

    remove clunky profiling

--- src/JaxPM2KY.py
+++ src/JaxPM2KY.py
@@ -30,15 +30,6 @@ import astropy.units as u
 from jax import config
 config.update("jax_enable_x64", True)
 
-# this can be a different code
-import cProfile
-import pstats
-from functools import wraps
-
-#class RelativeSeconds(lg.Formatter):
-#    def format(self, record):
-#        record.relativeCreated = record.relativeCreated // 1000
-#        return super().format(record)
 
 class RelativeSeconds(lg.Formatter):
     def format(self, record):
@@ -50,55 +41,6 @@ class RelativeSeconds(lg.Formatter):
         return super(RelativeSeconds, self).format(record)
 
 
-def profile(output_file=None, sort_by='cumulative', lines_to_print=None, strip_dirs=False):
-    """A time profiler decorator.
-    Inspired by and modified the profile decorator of Giampaolo Rodola:
-    http://code.activestate.com/recipes/577817-profile-decorator/
-    Args:
-        output_file: str or None. Default is None
-            Path of the output file. If only name of the file is given, it's
-            saved in the current directory.
-            If it's None, the name of the decorated function is used.
-        sort_by: str or SortKey enum or tuple/list of str/SortKey enum
-            Sorting criteria for the Stats object.
-            For a list of valid string and SortKey refer to:
-            https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats
-        lines_to_print: int or None
-            Number of lines to print. Default (None) is for all the lines.
-            This is useful in reducing the size of the printout, especially
-            that sorting by 'cumulative', the time consuming operations
-            are printed toward the top of the file.
-        strip_dirs: bool
-            Whether to remove the leading path info from file names.
-            This is also useful in reducing the size of the printout
-    Returns:
-        Profile of the decorated function
-    """
-
-    def inner(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            _output_file = output_file or func.__name__ + '.prof'
-            pr = cProfile.Profile()
-            pr.enable()
-            retval = func(*args, **kwargs)
-            pr.disable()
-            pr.dump_stats(_output_file)
-
-            with open(_output_file, 'w') as f:
-                ps = pstats.Stats(pr, stream=f)
-                if strip_dirs:
-                    ps.strip_dirs()
-                if isinstance(sort_by, (tuple, list)):
-                    ps.sort_stats(*sort_by)
-                else:
-                    ps.sort_stats(sort_by)
-                ps.print_stats(lines_to_print)
-            return retval
-
-        return wrapper
-
-    return inner
 
 ### input parameters (free)
 # currently this is just controlling the EDG part, later we want to also connect it to the HPM table
@@ -161,7 +103,6 @@ seed = 300
 #from functools import partial
 #@partial(jax.jit, static_argnums=[2])
 
-@profile(lines_to_print=5, output_file='log1.txt')
 @jax.jit
 def create_nbody(cosmo, cosmo2):
 
@@ -372,7 +313,6 @@ def HPM_GPmodel(rho,psi,a,logMmin=8,logMmax=16,NM=20,rmin=0.1,rmax=4,Nr=20):
     return np.asarray(10**model_T.mean()), np.asarray(10**model_P.mean())
 
 
-@profile(lines_to_print=5, output_file='log2.txt')
 #@jax.jit
 def particles_to_DeltaTP_mesh(res, i):
 
@@ -421,8 +361,6 @@ def particles_to_DeltaTP_mesh(res, i):
 
     return total_delta_egd, Tf, Pf
 
-# need to write an equivalent for tSZ
-@profile(lines_to_print=5, output_file='log3.txt')
 @jax.jit
 def tsz_Born(cosmo,
             pressure_planes,
